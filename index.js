a275da25a89d4f049ba2dc4341408b472121fd37
Fi Zilalil Huda <76860149+akiyamanico@users.noreply.github.com>
2024-02-29 23:27:18 +0700

    typo

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const multer = require("multer");
 const path = require('path');
 app.use(express.json());
 app.listen(3000,"0.0.0.0", () => {
-  console.log("Server running on port 8080");
+  console.log("Server running on port 3000");
 });
 
 const storage = multer.diskStorage({
